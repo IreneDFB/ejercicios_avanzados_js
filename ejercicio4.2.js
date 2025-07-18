d548d81eb299a1bcab9dd97703bd42a74a790c21
IreneDFB <irene_dfb@hotmail.com>
2025-07-18 19:34:58 +0200

    ejercicio 4.2

---
+++ ejercicio4.2.js
@@ -0,0 +1,15 @@
+function findArrayIndex(array, text) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === text) {
+      return i
+    }
+  }
+  return -1
+}
+function removeItem(array, text) {
+  const index = findArrayIndex(array, text)
+  if (index !== -1) {
+    array.splice(index, 1)
+  }
+  return array
+}
